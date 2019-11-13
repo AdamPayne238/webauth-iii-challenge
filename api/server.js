a1983f3c23385848a4b817cecd8900eb28f104c7
Adam Payne <ajpayne238@gmail.com>
2019-11-13 16:02:42 -0500

    configure middleware and api router set up

--- api/server.js
+++ api/server.js
@@ -1,21 +1,12 @@
 const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const apiRouter = require('./api-router');
+const configureMiddleware = require('./configure-middleware');
 
 const server = express();
 
-server.use(helmet());
-server.use(express.json());
-server.use(cors());
+configureMiddleware(server);
 
-server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
-
-server.get('/', (req, res) => {
-  res.send("It's alive!");
-});
+server.use('/api', apiRouter);
 
 module.exports = server;
